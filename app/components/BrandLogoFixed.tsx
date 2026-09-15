50fdfea9ca835d2b61452c43cddabe4c2be6aab2
pierrenavarre1 <Pierrenavarre1@gmail.com>
2026-09-15 16:47:52 -0500

    Use full-resolution SMRE logo source

--- app/components/BrandLogoFixed.tsx
+++ app/components/BrandLogoFixed.tsx
@@ -1,4 +1,4 @@
-const logo = 'https://img.leadsites.biz/images?format=avif&sig=v1%3Aa17bc12cba2b76934893814dcb64930a186b1a84f6da7f0b6941e14dc7a23f23&url=aHR0cHM6Ly9zMy5hbWF6b25hd3MuY29tL2VhcDAzLmVhc3lhZ2VudHByby5jb20vd3AtY29udGVudC91cGxvYWRzL3NpdGVzLzEyNTgvMjAyMi8wNi8wMTEyMjcwNS9TTVJFLWxvZ28td2hpdGUucG5n&w=300';
+const logo = 'https://s3.amazonaws.com/eap03.easyagentpro.com/wp-content/uploads/sites/1258/2022/06/01122705/SMRE-logo-white.png';
 
 export function BrandLogo({ className = '' }: { className?: string }) {
   return <img src={logo} alt="St. Mary’s Real Estate" className={className} />;
