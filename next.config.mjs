79101a1f567aae0ff68082831341cad84d71c544
pierrenavarre1 <Pierrenavarre1@gmail.com>
2026-09-15 17:08:46 -0500

    Allow current listing and team image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig={images:{remotePatterns:[{protocol:'https',hostname:'images.unsplash.com'}]}}; export default nextConfig;
+const nextConfig={images:{remotePatterns:[{protocol:'https',hostname:'images.unsplash.com'},{protocol:'https',hostname:'s3.amazonaws.com'},{protocol:'https',hostname:'images.homes.com'},{protocol:'https',hostname:'imagecdn.realty.com'}]}}; export default nextConfig;
